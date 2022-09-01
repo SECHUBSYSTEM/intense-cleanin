3fc588712eb65d235e508c1dce9600fc4526f88f
SECHUBSYSTEM <106301523+SECHUBSYSTEM@users.noreply.github.com>
2022-09-01 14:17:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+$(".package").on("click", function (e) {
+    $(".hidden").toggle(290);
+})
+
+$(".minus").on("click", function (e) {
+
+    var newVal = eval($(e.target).next().text() + "-1");
+    if (parseInt(newVal) >= 0) $(e.target).next().text(newVal);
+    else return
+    update()
+})
+
+$(".plus").on("click", function (ev) {
+
+    var newVal = eval($(ev.target).prev().text() + "+1");
+    if (parseInt(newVal) <= 5)
+        $(ev.target).prev().text(newVal);
+    else return
+    update()
+
+})
+
+function update() {
+    var noOfBedrooms, noOfBathrooms, container = $("#rooms").children();
+    noOfBedrooms = container.eq(2).text();
+    noOfBathrooms = container.eq(6).text();
+
+    $("#valueD").text(noOfBedrooms);
+    $("#valueD2").text(noOfBathrooms);
+
+    var bedroomCost, bathroomCost;
+    if (parseInt(noOfBedrooms) > 1) {
+        var bedroomNet = eval(noOfBedrooms + "-1");
+        bedroomCost = eval("190+" + bedroomNet + "*100");
+    } else if (parseInt(noOfBedrooms) == 0) {
+        bedroomCost = "0";
+    } else {
+        bedroomCost = "190";
+    }
+
+    if (parseInt(noOfBathrooms) > 1) {
+        var bathroomNet = eval(noOfBathrooms + "-1");
+        bathroomCost = eval("190+" + bathroomNet + "*80");
+    } else if (parseInt(noOfBathrooms) == 0) {
+        bathroomCost = "0";
+    } else {
+        bathroomCost = "190";
+    }
+
+    var totalCost = eval(bedroomCost + "+" + bathroomCost);
+    $("#total").text(totalCost);
+}
